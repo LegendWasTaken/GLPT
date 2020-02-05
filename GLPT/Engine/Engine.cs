a86bdbd8541bc1a6bd7b218a6d6076e5d481adfe
LegendWasTaken <caiodcw@gmail.com>
2020-02-04 20:05:43 -0800

    Merged window and engine class

--- GLPT/Engine/Engine.cs
+++ GLPT/Engine/Engine.cs
@@ -1,24 +1,74 @@
-﻿using osuTK.Graphics.OpenGL4;
+﻿using System;
+using System.Drawing;
+using osuTK;
+using osuTK.Graphics.OpenGL4;
 
 namespace GLPT
 {
-    public class Engine
+    public class Engine : GameWindow
     {
-        private Window _window;
         private Scene _scene;
-
+        private Shader _shader;
+        
         public Engine(int width, int height, Scene scene)
         {
-            _window = new Window("C# Path Tracer", width, height);
+            Title = "GL Path Tracer";
+            Width = width;
+            Height = height;
             _scene = scene;
+            _shader = new Shader();
         }
         
         public void Initialize()
         {
-            // do start stuff lol
-            // Start the window instance
-            _window.Run();
+            Run();
         }
         
+        protected override void OnLoad(EventArgs e)
+        {
+            // Start up
+            DebugMessageHandler.Initialize(); // Starting the debug message handler 
+
+            // Setup viewport stuff
+            GL.Viewport(0, 0, Width, Height);
+            
+            // Create the full screen triangle
+            new VBO(
+                new Vector3(-5f, -1f, 0f),
+                new Vector3(1f, -1f, 0f),
+                new Vector3(1f, 5f, 0)
+            );
+            
+            // Initialize shaders
+            _shader.Create();
+            
+            _shader.setUniform("res", new Vector2(Width, Height));
+            _shader.setUniform("color", new Vector3(0, 255, 255));
+            _shader.setUniform("camera_location", new Vector3(0, 0, 0));
+            
+            _shader.setUniform("viewMatrix", Matrix4.Identity);
+            // _shader.setUniform("res", new Vector2(Width, Height));
+
+            base.OnLoad(e); // dont fucking touch this
+        }
+        
+        protected override void OnRenderFrame(FrameEventArgs e)
+        {
+            // Game loop
+            GL.ClearColor(Color.Black);
+            GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
+            /*=====================*/
+            _shader.useShader();
+            GL.DrawArrays(PrimitiveType.Triangles, 0, 3);
+            /*=====================*/
+            SwapBuffers();
+            base.OnRenderFrame(e);
+        }
+
+        public override void Exit()
+        {
+            _shader.Release();
+            base.Exit();
+        }
     }
 }
